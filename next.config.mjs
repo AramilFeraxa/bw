aaa0e0e17f5967488009f33351f5028db0eda49b
AramilFeraxa <aferaxa@gmail.com>
2025-10-10 14:55:21 +0200

    next

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   trailingSlash: true,
   //assetPrefix: './',
   images: {
     unoptimized: true,
   },
+  i18n: undefined,
 };
 
 export default nextConfig;
