0f3bd378cdf054730c4d311242b2249702fa3803
AramilFeraxa <aferaxa@gmail.com>
2025-11-06 12:02:58 +0100

    +projects

---
+++ src/data/projects/swiadomi-nauczyciele.js
@@ -0,0 +1,98 @@
+export const swiadomiNauczyciele = {
+    id: "swiadomi-nauczyciele-moca-opolszczyzny",
+    seoTitle: "Świadomi Nauczyciele Mocą Opolszczyzny | Bezpieczny Wypoczynek",
+    seoDescription:
+        "Projekt wzmacniający kompetencje nauczycieli województwa opolskiego w zakresie bezpieczeństwa psychicznego, prawnego i organizacyjnego wyjazdów szkolnych oraz wdrażania Standardów Ochrony Małoletnich.",
+    title: "Świadomi Nauczyciele Mocą Opolszczyzny",
+    subtitle: "",
+    category: "Edukacja",
+    heroImage: "/assets/images/projects/swiadomi-nauczyciele-hero.jpg",
+    progressPercent: 75,
+    intro: [
+        'Projekt zrealizowany przez Stowarzyszenie Aktywni na Rzecz Bezpiecznego Wypoczynku miał na celu wzmocnienie kompetencji nauczycieli województwa opolskiego w zakresie bezpieczeństwa psychicznego, prawnego i organizacyjnego wyjazdów szkolnych oraz wdrażania Standardów Ochrony Małoletnich w kontekście przebywania uczniów poza szkołą w ramach wycieczek z noclegiem i dłuższych wyjazdów tzw. "zielonych szkół".',
+        'W ramach zadania przeprowadzono dwa szkolenia pod nazwą "Bezpieczny Wypoczynek" w Zamku Moszna - 21 i 28 października 2025 r. Wzięło w nich udział 40 nauczycieli szkół podstawowych i ponadpodstawowych z całej Opolszczyzny. Spotkania łączyły teorię, praktykę i wymianę doświadczeń.',
+    ],
+    sections: [
+        {
+            heading: "Program konferencji",
+            paragraphs: [
+                'Program obejmował zagadnienia z zakresu rozpoznawania "czerwonych flag" u uczniów, zasad pierwszej pomocy psychicznej, obowiązków wynikających z tzw. "lex Kamilek" oraz wdrażania Standardów Ochrony Małoletnich w szkołach i podczas wyjazdów z noclegiem.',
+                <>Nauczyciele poznali także program regionalny <strong>"Opolskie. Stąd pochodzę!"</strong>, promujący wartości edukacyjne i integracyjne wycieczek szkolnych w regionie oraz zapoznali się z Zamkiem Moszna, jako zarówno jedną z największych atrakcji woj. opolskiego, jak i doskonałym centrum turystyki aktywnej i fabularnej dla dzieci i młodzieży szkolnej.</>,
+            ],
+        },
+        {
+            heading: "Konferencje Bezpieczny Wypoczynek",
+            paragraphs: [
+                "Obie konferencje połączyły wiedzę ekspercką z praktyką nauczycieli. Warsztaty dotyczyły m.in. rozpoznawania stanów kryzysowych, planu pierwszej pomocy psychicznej i wdrażania procedur SOM.",
+                "Uczestnicy otrzymali materiały szkoleniowe, checklisty i certyfikaty, a także zaproszenie do wolontariatu w ramach Stowarzyszenia.",
+                <><strong>Badanie efektywności wykazało, że 100% uczestników uzyskało wyższy wynik w post-teście niż w pre-teście</strong>, a średni przyrost wiedzy wyniósł 76,9%, szczególnie w obszarach pierwszej pomocy psychicznej (82,35%) i rozpoznawania kryzysów emocjonalnych (80,37%)."</>,
+            ],
+        },
+    ],
+    results: {
+        heading: "Rezultaty i znaczenie projektu",
+        items: [
+            "Pełna realizacja celów i przekroczenie wskaźników",
+            "Wzmocnienie kompetencji i satysfakcja uczestników",
+            "Trwały efekt społeczny i rozwój Stowarzyszenia",
+        ],
+    },
+    images: [
+        {
+            src: "/assets/images/projects/swiadomi-nauczyciele-1.jpg",
+            alt: "Konferencja Bezpieczny Wypoczynek - uczestnicy",
+        },
+        {
+            src: "/assets/images/projects/swiadomi-nauczyciele-2.jpg",
+            alt: "Warsztaty w Zamku Moszna",
+        },
+        {
+            src: "/assets/images/projects/swiadomi-nauczyciele-3.jpg",
+            alt: "Materiały szkoleniowe",
+        },
+        {
+            src: "/assets/images/projects/swiadomi-nauczyciele-4.jpg",
+            alt: "Uczestnicy podczas warsztatów",
+        },
+    ],
+    downloadFiles: [
+        {
+            label: "Pobierz materiały z konferencji (ZIP)",
+            url: "/downloads/materialy-konferencja-bezpieczny-wypoczynek.zip",
+        },
+    ],
+    coordinators: [
+        {
+            name: "Lena Szpyrka",
+            email: "",
+        }
+    ],
+    funding: {
+        description: "Projekt finansowany",
+        funders: [
+            {
+                name: "ze środków Narodowego Instytutu Wolności - Centrum Rozwoju Społeczeństwa Obywatelskiego w ramach rządowego programu wsparcia organizacji pozarządowych Moc Małych Społeczności",
+                logo: "/assets/images/projects/funders/urzad-marszalkowski-opole.png",
+                website: "https://www.umwo.opole.pl/",
+            },
+        ],
+    },
+    partners: [
+        {
+            name: "HY",
+            logo: "/assets/images/projects/partners/hy-logo.png",
+            website: "https://hugoyorck.pl/",
+        },
+        {
+            name: "Zamek Moszna",
+            logo: "/assets/images/projects/partners/zamek-moszna-logo.png",
+            website: "",
+        },
+    ],
+    shortAbout: {
+        author: "Paulina Jędrzejak-Bartela",
+        date: "28 października 2025 r.",
+        authorImage: "/assets/images/team/paulina-jedrzejak-bartela.jpg",
+        text: 'Chciałam podziękować nauczycielom, że, mimo takiego trudnego czasu, jeśli chodzi o organizację wycieczek szkolnych - z uwagi na, nazwijmy to, piętrzące się niejasności związane z wynagrodzeniem nauczycieli - przybyli na konferencję "Bezpieczny wypoczynek" do Zamku Moszna. Myślę, że uczestnicy wychodzili zadowoleni. Otrzymali naprawdę przygotowaną porcję wiedzy takiej praktycznej, związanej z zapewnieniem bezpieczeństwa w czasie wyjazdów nie tylko uczniom, ale również sobie - jako opiekunom, którzy odpowiadają za życie i zdrowie swoich podopiecznych. Takie spotkania są naszym zdaniem, mówię tu w imieniu członków i wolontariuszy Stowarzyszenia, bardzo ważne i niezwykle potrzebne. Budują wspólnotę wsparcia, wsparcia również w tych trudniejszych chwilach (oby było ich jak najmniej) jak nieszczęśliwe wypadki, a to, że możemy spotkać się czy nawet, jak dostałam zapewnienie od Prezesa Zamku, p. Tomasza Ganczarka, spotykać w Zamku Moszna jest wyjątkowe. Móc mówić o bezpiecznym wypoczynku w największej atrakcji turystycznej naszego województwa, to naprawdę wieli zaszczyt dla nas. Raz jeszcze dziękuję nauczycielom i władzom zamku, prelegentom i wszystkim, którzy uczynili te konferencje dobrymi wydarzeniami dla naszej społeczności.',
+    },
+};
