deeebdc23aed887ba99050386e79715f7363b816
AramilFeraxa <aferaxa@gmail.com>
2025-12-16 13:09:35 +0100

    +GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -17,8 +18,19 @@ export default function Document() {
         <link rel="stylesheet" href="/assets/vendors/fontawesome/css/all.min.css" />
         <link rel="stylesheet" href="/assets/vendors/paroti-icons/style.css" />
         <link rel="stylesheet" href="/assets/css/paroti.css" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T6VQKZWK');`
+        }} />
+        {/* End Google Tag Manager */}
       </Head>
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6VQKZWK"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <Main />
         <NextScript />
       </body>
