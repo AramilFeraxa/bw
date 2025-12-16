5bc7704f90775256c7e4c36c2628fd4a7ca31064
AramilFeraxa <aferaxa@gmail.com>
2025-12-16 13:12:19 +0100

    fix

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,7 +30,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </Head>
       <body>
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T6VQKZWK"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         <Main />
         <NextScript />
       </body>
